d96100cbe5568f23bc16dd0139ecb8206c851f1b
ShadrikovaPolina <35374835+ShadrikovaPolina@users.noreply.github.com>
2019-01-18 00:00:23 +0400

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using Blog.Models;
+using Blog.Service;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CrudCategory categoryService;
+
+        public CategoryController()
+        {
+            string dataStore = ConfigurationManager.AppSettings["DataStore"].ToString();
+            switch (dataStore)
+            {
+                case "DB":
+                    categoryService = new CategoryService();
+                    break;
+                case "File":
+                    categoryService = new CategoryFileService();
+                    break;
+            }
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ActionResult EditCategory(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (categoryService.findCategoryId(id) != null)
+            {
+                return View(categoryService.findCategoryId(id));
+            }
+            return HttpNotFound();
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            categoryService.Edit(category);
+            return RedirectToAction("Categories");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            categoryService.Create(category);
+            return RedirectToAction("Categories");
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult DeleteCategory(int id)
+        {
+            categoryService.Delete(id);
+            return RedirectToAction("Categories");
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Categories()
+        {
+            return View(categoryService.getList());
+        }
+    }
+}
---
+++ Controllers/PostController.cs
@@ -0,0 +1,105 @@
+﻿using Blog.Models;
+using Blog.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        CrudPost postService;
+
+        public PostController()
+        {
+            string dataStore = ConfigurationManager.AppSettings["DataStore"].ToString();
+            switch (dataStore)
+            {
+                case "DB":
+                    postService = new PostService();
+                    break;
+                case "File":
+                    postService = new PostFileService();
+                    break;
+            }
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (postService.findPostId(id) != null)
+            {
+                ViewBag.Categories = postService.DropDownEdit(id);
+                return View(postService.findPostId(id));
+            }
+            return HttpNotFound();
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public ActionResult UpdatePost(Post post)
+        {
+            postService.Edit(post);
+            return RedirectToAction("Posts");
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ActionResult CreatePost()
+        {
+            ViewBag.Posts = postService.DropDownCreate();
+            //  ViewBag.Schools = schools;
+            return View();
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public ActionResult CreatePost(Post post)
+        {
+            postService.Create(post);
+            return RedirectToAction("Posts");
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult DeletePost(int id)
+        {
+            postService.Delete(id);
+            return RedirectToAction("Posts");
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Posts()
+        {
+            return View(postService.getList());
+        }
+
+     /*   [HttpGet]
+        public ActionResult PostsSearch(int? searchingCategory)
+        {
+            ViewBag.searchingCategory = postService.DropDownCreate();
+            return View(postService.GetPost(searchingCategory));
+        }*/
+
+        [HttpGet]
+        public ActionResult NewBlog(Category category)
+        {
+            ViewBag.Categories = postService.getListCategories();
+            return View(postService.getList());
+        }
+
+        [HttpPost]
+        public ActionResult PostSearch(int? searchingPost)
+        {
+            return View(postService.GetPost(searchingPost));
+        }
+    }
+}
