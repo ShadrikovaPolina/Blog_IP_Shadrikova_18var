93ccf5d7887c8cbeb73920fdac7829a0d2619c4b
ShadrikovaPolina <35374835+ShadrikovaPolina@users.noreply.github.com>
2019-01-17 23:52:49 +0400

    Add files via upload

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Date { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
