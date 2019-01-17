61b2977820da092e338dfc267599f289b6c841ca
ShadrikovaPolina <35374835+ShadrikovaPolina@users.noreply.github.com>
2019-01-17 23:58:37 +0400

    Add files via upload

---
+++ Service/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Blog.Service
+{
+    public class CategoryService : CrudCategory
+    {
+        DatabaseBlogContext db = new DatabaseBlogContext();
+
+        public override void Delete(int id)
+        {
+            Category b = db.Categories.Find(id);
+            if (b != null)
+            {
+                db.Categories.Remove(b);
+                db.SaveChanges();
+            }
+        }
+
+        public override void Edit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public override void Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        public override Category findCategoryId(int? id)
+        {
+            Category category = db.Categories.Find(id);
+            return category;
+        }
+
+        public override List<Category> getList()
+        {
+            return db.Categories.ToList();
+        }
+    }
+}
---
+++ Service/PostService.cs
@@ -0,0 +1,71 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Blog.Service
+{
+    public class PostService : CrudPost
+    {
+        DatabaseBlogContext db = new DatabaseBlogContext();
+
+        public override void Delete(int id)
+        {
+            Post b = db.Posts.Find(id);
+            if (b != null)
+            {
+                db.Posts.Remove(b);
+                db.SaveChanges();
+            }
+        }
+
+        public override void Edit(Post post)
+        {
+            db.Entry(post).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public override void Create(Post post)
+        {
+            db.Posts.Add(post);
+            db.SaveChanges();
+        }
+
+        public override Post findPostId(int? id)
+        {
+            Post post = db.Posts.Find(id);
+            return post;
+        }
+
+        public override List<Post> getList()
+        {
+            return db.Posts.ToList();
+        }
+
+        public override SelectList DropDownCreate()
+        {
+            SelectList categories = new SelectList(db.Categories, "Id", "Name");
+            return categories;
+        }
+
+        public override SelectList DropDownEdit(int? id)
+        {
+            Post post = db.Posts.Find(id);
+            SelectList categories = new SelectList(db.Categories, "Id", "Name", post.CategoryId);
+            return categories;
+        }
+
+        public override List<Post> GetPost(int? searchingPost)
+        {
+            var result = db.Posts.Where(x => x.CategoryId == searchingPost).ToList();
+            return result;
+        }
+
+        public override List<Category> getListCategories()
+        {
+            return db.Categories.ToList();
+        }
+    }
+}
